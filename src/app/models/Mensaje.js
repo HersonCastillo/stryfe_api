5cd073b00adf19256f014707162a9e85fdefd783
Herson Castillo <hersoncastillo12@hotmail.com>
2018-09-10 11:47:28 -0600

    fix

--- src/app/models/Mensaje.js
+++ src/app/models/Mensaje.js
@@ -5,7 +5,7 @@ var Sequelize = require('sequelize');
 const Table = sequelize.define(tableName, {
     id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
     mensaje: Sequelize.TEXT(150),
-    id_usuario: Sequelize.INTEGER,
+    id_usuario: { type: Sequelize.INTEGER, allowNull: true },
     id_estado_mensaje: Sequelize.INTEGER
 });
 
