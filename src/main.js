355db727d332e43fbfd6ad903508b916ad27da62
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-10-02 23:01:20 -0600

    engine jade

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var passport = require('passport');
 require('./app/utils/passport');
 
@@ -10,22 +9,30 @@ var api = require('./app/routes/index');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var mailer = require('express-mailer');
 var port = 3500;
+
+mailer.extend(app, {
+    from: 'no-reply@stryfe.com',
+    host: 'smtp.gmail.com',
+    secureConnection: true,
+    port: 465,
+    transportMethod: 'SMTP',
+    auth: {
+        user: 'stryfecompany@gmail.com',
+        pass: 'ABC12345#'
+    }
+});
 
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
 app.use('/auth', auth);
 app.use('/api/v1', passport.authenticate('jwt', { session: false }), api);
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST,PUT,DELETE,OPTIONS,PURGE,GET");
-    next();
-});
 
-app.get('*', (req, res, next) => {
+app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
@@ -33,4 +40,4 @@ io.on('connection', (socket) => {
     //start real time with [socket]...
 });
 
-http.listen(port, () => console.log('Contectado...'));
+http.listen(port, () => console.log(`Contectado [::${port}]`));
