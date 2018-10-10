fa86d8a74060af750d2f9392c6170b5fd5f6f200
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-10-10 17:31:11 -0600

    reorder

--- src/main.js
+++ src/main.js
@@ -1,52 +1,54 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const token = require('./app/utils/api.sql').tokenKey;
-const jwt = require('express-jwt');
-const multipart = require('connect-multiparty');
-require('./app/utils/passport');
+(function () {
+    'use strict';
+    const path = require('path');
+    const jwt = require('express-jwt');
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const multipart = require('connect-multiparty');
+    const token = require('./app/utils/api.sql').tokenKey;
+    require('./app/utils/passport');
+    const app = express();
+    const auth = require('./app/routes/auth');
+    const api = require('./app/routes/index');
+    const http = require('http').Server(app);
+    const io = require('socket.io')(http);
+    const mailer = require('express-mailer');
+    const port = 3500;
 
-const auth = require('./app/routes/auth');
-const api = require('./app/routes/index');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const mailer = require('express-mailer');
-const port = 3500;
+    mailer.extend(app, {
+        from: 'no-reply@stryfe.com',
+        host: 'smtp.gmail.com',
+        secureConnection: true,
+        port: 465,
+        transportMethod: 'SMTP',
+        auth: {
+            user: 'stryfecompany@gmail.com',
+            pass: 'ABC12345#'
+        }
+    });
 
-mailer.extend(app, {
-    from: 'no-reply@stryfe.com',
-    host: 'smtp.gmail.com',
-    secureConnection: true,
-    port: 465,
-    transportMethod: 'SMTP',
-    auth: {
-        user: 'stryfecompany@gmail.com',
-        pass: 'ABC12345#'
-    }
-});
+    app.use(multipart());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.static(path.join(__dirname, 'dist')));
+    app.set('views', path.join(__dirname, 'app/views'));
+    app.set('view engine', 'jade');
+    app.use('/auth', auth);
+    app.use('/api/v1', jwt({ secret: token }), api);
+    app.use(function (err, req, res, next) {
+        if (err.name === 'UnauthorizedError') {
+            return res.status(403).send({
+                error: 'Llave de acceso no permitida.'
+            });
+        }
+    });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
 
-app.use(multipart());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'jade');
-app.use('/auth', auth);
-app.use('/api/v1', jwt({ secret: token }), api);
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        return res.status(403).send({
-            error: 'Llave de acceso no permitida.'
-        });
-    }
-});
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+    io.on('connection', (socket) => {
+        //start real time with [socket]...
+    });
 
-io.on('connection', (socket) => {
-    //start real time with [socket]...
-});
-
-http.listen(port, () => console.log(`Contectado [:${port}]`));
+    http.listen(port, () => console.log(`Contectado [:${port}]`));
+})();
